87f7f9600293d696f918db85d1c3cda483c52f78
West Kraemer <WestKraemer@gmail.com>
2017-01-23 17:39:38 -0500

    add table view and table view controller text

--- toDoList/FirstViewController.swift
+++ toDoList/FirstViewController.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+       return 1
+        
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "test"
+        
+        return cell
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
